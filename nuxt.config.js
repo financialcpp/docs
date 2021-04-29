f42066ca337e886fc3a79e77a87673f0d73ca596
Will Hoyle <will.hoyle@gmail.com>
2021-04-29 17:18:37 -0400

    Remove console.log statement

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,4 @@
 import theme from '@nuxt/content-theme-docs'
-console.log(theme({
-    docs: {
-        primaryColor: '#9f7aea'
-    },
-    content: {
-        markdown: {
-            prism: {
-                theme: './assets/css/prism-nord.css'
-            }
-        }
-    },
-}));
 export default theme({
     docs: {
         primaryColor: '#9f7aea'
